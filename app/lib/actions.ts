37c9803bb25f6612286a5608e15773243d8d9ab4
Biswajit Oraon <biswajitoraon19@gmail.com>
2024-08-28 22:14:11 +0530

    fix: credential signIn

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -119,7 +119,7 @@ export async function authenticate(
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
-        case "CredentialsSignIn":
+        case "CredentialsSignin":
           return "Invalid credentials";
         default:
           return "Something went wrong.";
